6d58b388aeb2a4ff9816c686835c3d7b6d56f2a1
Ujjwal Kumar Mishra <140422122+ujju787@users.noreply.github.com>
2025-06-29 16:20:47 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gc-2+zri)uvillc&gs-*h(irr-*x*yj0qx%1vg8v0%nj3kh40f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
